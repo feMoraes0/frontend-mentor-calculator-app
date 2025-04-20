2a5fe0e4c9e9a2b8c104a7372e92de8a9ff687c0
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-04-20 20:54:05 -0300

    feat: add util with numpad button labels

---
+++ src/utils/numpad.ts
@@ -0,0 +1,20 @@
+export const numpadNumbers = [
+  7,
+  8,
+  9,
+  'DEL',
+  4,
+  5,
+  6,
+  '+',
+  1,
+  2,
+  3,
+  '-',
+  '.',
+  0,
+  '/',
+  'x',
+];
+
+export const numpadEnd = ['RESET', '='];
